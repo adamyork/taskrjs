5ef6abdcfbfe1487138052c0d46629247f93a034
Adam York <adamyork@users.noreply.github.com>
2017-10-20 20:59:12 -0400

    fixing readme

--- build.js
+++ build.js
@@ -9,11 +9,11 @@ var fs = require('fs');
 fs.readFile('index.html', 'utf8', function(err, contents) {
     let uri = 'data:text/html,' + encodeURIComponent(contents);
     fs.readFile('README.md', 'utf8', (err, contents) => {
-        let start = contents.indexOf('```');
-        let end = contents.lastIndexOf('```');
+        let start = contents.indexOf('<pre>');
+        let end = contents.lastIndexOf('</pre>');
         let left = contents.slice(0, start);
-        let right = contents.slice(end + 3, contents.length);
-        let output = left + '```' + uri + '```' + right;
+        let right = contents.slice(end + 6, contents.length);
+        let output = left + '<pre>' + uri + '</pre>' + right;
         fs.writeFile('README.md', output, (err) => {
             if (err) {
                 throw err;
